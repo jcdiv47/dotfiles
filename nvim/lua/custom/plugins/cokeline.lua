a1c7a3d15c4c8dd5af77a70b88ed13cb88265add
jcdiv47 <jackchai2.0@gmail.com>
2024-06-06 22:19:49 +0800

    [nvim] added plugin cokeline.nvim

--- nvim/lua/custom/plugins/cokeline.lua
+++ nvim/lua/custom/plugins/cokeline.lua
@@ -6,8 +6,11 @@ return {
     'stevearc/resession.nvim', -- Optional, for persistent history
   },
   config = function()
+    local is_picking_focus = require('cokeline.mappings').is_picking_focus
+    local is_picking_close = require('cokeline.mappings').is_picking_close
     local get_hex = require('cokeline.hlgroups').get_hl_attr
 
+    local red = vim.g.terminal_color_1
     local yellow = vim.g.terminal_color_3
 
     -- TODO: add keymap for closing currently focused buffer
@@ -17,7 +20,69 @@ return {
         fg = function(buffer)
           return buffer.is_focused and get_hex('Normal', 'fg') or get_hex('Comment', 'fg')
         end,
-        bg = get_hex('ColorColumn', 'bg'),
+        bg = function()
+          return get_hex('ColorColumn', 'bg')
+        end,
+      },
+
+      components = {
+        {
+          text = function(buffer)
+            return (buffer.index ~= 1) and '▏' or ''
+          end,
+        },
+        {
+          text = '  ',
+        },
+        {
+          text = function(buffer)
+            return (is_picking_focus() or is_picking_close()) and buffer.pick_letter .. ' ' or buffer.devicon.icon
+          end,
+          fg = function(buffer)
+            return (is_picking_focus() and yellow) or (is_picking_close() and red) or buffer.devicon.color
+          end,
+          italic = function()
+            return (is_picking_focus() or is_picking_close())
+          end,
+          bold = function()
+            return (is_picking_focus() or is_picking_close())
+          end,
+        },
+        -- {
+        --   text = ' ',
+        -- },
+        {
+          text = function(buffer)
+            return buffer.filename .. '  '
+          end,
+          bold = function(buffer)
+            return buffer.is_focused
+          end,
+        },
+        {
+          text = '',
+          on_click = function(_, _, _, _, buffer)
+            buffer:delete()
+          end,
+        },
+        {
+          text = '  ',
+        },
+      },
+      pick = {
+        -- Whether to use the filename's first letter first before
+        -- picking a letter from the valid letters list in order.
+        -- default: `true`
+        ---@type boolean
+        use_filename = true,
+
+        -- The list of letters that are valid as pick letters. Sorted by
+        -- keyboard reachability by default, but may require tweaking for
+        -- non-QWERTY keyboard layouts.
+        -- default: `'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERTYQP'`
+        -- TODO: change it for Colemak-DH layout
+        ---@type string
+        letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERTYQP',
       },
       sidebar = {
         filetype = { 'NvimTree', 'neo-tree' },
@@ -34,49 +99,16 @@ return {
           },
         },
       },
-      components = {
-
-        {
-          text = function(buffer)
-            return ' ' .. buffer.devicon.icon
-          end,
-          fg = function(buffer)
-            return buffer.devicon.color
-          end,
-        },
-        {
-          text = function(buffer)
-            return buffer.unique_prefix
-          end,
-          fg = get_hex('Comment', 'fg'),
-          italic = true,
-        },
-        {
-          text = function(buffer)
-            return buffer.filename .. ' '
-          end,
-          underline = function(buffer)
-            return buffer.is_hovered and not buffer.is_focused
-          end,
-          bold = function(buffer)
-            return buffer.is_focused
-          end,
-          italic = function(buffer)
-            return buffer.is_focused
-          end,
-        },
-        {
-          text = ' ' .. '',
-          on_click = function(_, _, _, _, buffer)
-            buffer:delete()
-          end,
-        },
-      },
     }
     local map = vim.api.nvim_set_keymap
     map('n', '<S-Tab>', '<Plug>(cokeline-focus-prev)', { silent = true })
     map('n', '<Tab>', '<Plug>(cokeline-focus-next)', { silent = true })
-    map('n', '<Leader>p', '<Plug>(cokeline-switch-prev)', { desc = 'switch buffer forward', silent = true })
-    map('n', '<Leader>n', '<Plug>(cokeline-switch-next)', { desc = 'switch buffer backward', silent = true })
+    map('n', '<leader>sp', '<Plug>(cokeline-switch-prev)', { desc = '[S]witch buffer backward', silent = true })
+    map('n', '<leader>sn', '<Plug>(cokeline-switch-next)', { desc = '[S]witch buffer forward', silent = true })
+    map('n', '<leader>pf', '<Plug>(cokeline-pick-focus)', { desc = '[F]ocus a buffer by its `[P]ick_letter`', silent = true })
+    map('n', '<leader>pc', '<Plug>(cokeline-pick-close)', { desc = '[C]lose a buffer by its `[P]ick_letter`', silent = true })
+    vim.keymap.set('n', '<leader>bp', function()
+      require('cokeline.mappings').pick 'focus'
+    end, { desc = 'Pick a buffer to focus' })
   end,
 }
