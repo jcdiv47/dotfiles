9225534145574398f7cf91802e28d6b952640b6a
jcdiv47 <45190310+jcdiv47@users.noreply.github.com>
2025-04-18 15:38:56 +0800

    Update .wezterm.lua

--- windows/wezterm/.wezterm.lua
+++ windows/wezterm/.wezterm.lua
@@ -17,77 +17,19 @@ local act = wezterm.action
 -- Note that the leader `CTRL+a` overwrites the same keymap for increasing a number in Neovim
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-  {
-    key = "_",
-    mods = "LEADER|SHIFT",
-    action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
-  },
-  {
-    key = "|",
-    mods = "LEADER|SHIFT",
-    action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
-  },
-  {
-    key = "F9",
-    mods = "CTRL",
-    action = act.ShowTabNavigator,
-  },
-  {
-    key = "[",
-    mods = "CTRL",
-    action = act.ActivateTabRelative(-1),
-  },
-  {
-    key = "]",
-    mods = "CTRL",
-    action = act.ActivateTabRelative(1),
-  },
-  {
-    key = "h",
-    mods = "CTRL|SHIFT",
-    action = act.AdjustPaneSize { "Left", 5 },
-  },
-  {
-    key = "j",
-    mods = "CTRL|SHIFT",
-    action = act.AdjustPaneSize { "Down", 5 },
-  },
-  {
-    key = "k",
-    mods = "CTRL|SHIFT",
-    action = act.AdjustPaneSize { "Up", 5 },
-  },
-  {
-    key = "l",
-    mods = "CTRL|SHIFT",
-    action = act.AdjustPaneSize { "Right", 5 },
-  },
-  {
-    key = "h",
-    mods = "CTRL",
-    action = act.ActivatePaneDirection "Left",
-  },
-  {
-    key = "j",
-    mods = "CTRL",
-    action = act.ActivatePaneDirection "Down",
-  },
-  {
-    key = "k",
-    mods = "CTRL",
-    action = act.ActivatePaneDirection "Up",
-  },
-  { -- foo bar
-    key = "l",
-    mods = "CTRL",
-    action = act.ActivatePaneDirection "Right",
-  },
+  { key = "_", mods = "LEADER|SHIFT", action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
+  { key = "|", mods = "LEADER|SHIFT", action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
+  { key = "F9", mods = "CTRL", action = act.ShowTabNavigator },
+  { key = "[", mods = "CTRL", action = act.ActivateTabRelative(-1) },
+  { key = "]", mods = "CTRL", action = act.ActivateTabRelative(1) },
+  { key = "h", mods = "CTRL|SHIFT", action = act.AdjustPaneSize { "Left", 5 } },
+  { key = "j", mods = "CTRL|SHIFT", action = act.AdjustPaneSize { "Down", 5 } },
+  { key = "k", mods = "CTRL|SHIFT", action = act.AdjustPaneSize { "Up", 5 } },
+  { key = "l", mods = "CTRL|SHIFT", action = act.AdjustPaneSize { "Right", 5 } },
   -- create new tab
-  {
-    key = "t",
-    mods = "CTRL",
-    action = act.SpawnTab "CurrentPaneDomain",
-  },
+  { key = "t", mods = "CTRL", action = act.SpawnTab "CurrentPaneDomain" },
+  -- activate pane selection mode with the default alphabet (labels are "a", "s", "d", "f" and so on)
+	{ key = "1", mods = "CTRL", action = act.PaneSelect },
 }
 
 -- and finally, return the configuration to wezterm
