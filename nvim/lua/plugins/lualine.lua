1b8ad093406072220ba1fb50364a0109e8dea836
jcdiv47 <45190310+jcdiv47@users.noreply.github.com>
2025-11-26 00:52:37 +0800

    Modify lualine config
    Remove winbar in lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -69,20 +69,20 @@ return {
       -- cond = hide_in_width,
     }
 
-    local tabs = {
-      "tabs",
-      tab_max_length = 40, -- Maximum width of each tab. The content will be shorten dynamically (example: apple/orange -> a/orange)
-      max_length = vim.o.columns / 3, -- Maximum width of tabs component.
-      -- Note:
-      -- It can also be a function that returns
-      -- the value of `max_length` dynamically.
-      mode = 0, -- 0: Shows tab_nr
-      tabs_color = {
-        -- Same values as the general color option can be used here.
-        active = "lualine_a_normal", -- Color for active tab.
-        inactive = "lualine_b_normal", -- Color for inactive tab.
-      },
-    }
+    -- local tabs = {
+    --   "tabs",
+    --   tab_max_length = 40, -- Maximum width of each tab. The content will be shorten dynamically (example: apple/orange -> a/orange)
+    --   max_length = vim.o.columns / 3, -- Maximum width of tabs component.
+    --   -- Note:
+    --   -- It can also be a function that returns
+    --   -- the value of `max_length` dynamically.
+    --   mode = 0, -- 0: Shows tab_nr
+    --   tabs_color = {
+    --     -- Same values as the general color option can be used here.
+    --     active = "lualine_a_normal", -- Color for active tab.
+    --     inactive = "lualine_b_normal", -- Color for inactive tab.
+    --   },
+    -- }
 
     local filename = {
       "filename",
@@ -109,15 +109,15 @@ return {
       sections = {
         lualine_a = { mode },
         lualine_b = { branch, diff, "diagnostics" },
-        lualine_c = { "filename" },
+        lualine_c = { filename },
         lualine_x = {},
         lualine_y = { lsp_status, "progress" },
         lualine_z = { "location" },
       },
-      winbar = {
-        lualine_y = { filename },
-        lualine_z = { tabs },
-      },
+      -- winbar = {
+      --   lualine_y = { filename },
+      --   lualine_z = { tabs },
+      -- },
     })
   end,
 }
