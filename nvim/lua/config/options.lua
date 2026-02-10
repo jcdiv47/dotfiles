ac10ef63d55c250552ac88b68a0c46425f46dbf3
jcdiv47 <45190310+jcdiv47@users.noreply.github.com>
2026-02-10 14:39:00 +0800

    Update options for swapfile and clipboard settings
    Disable swap files and configure clipboard settings.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,18 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = true
+vim.opt.swapfile = false -- Disable swap files
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
