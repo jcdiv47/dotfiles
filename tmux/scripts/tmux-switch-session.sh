7931bd6c4e9121c52680cd3d714f94eaeb1f934f
jcdiv47 <jackchai2.0@gmail.com>
2026-02-10 15:09:26 +0800

    Updated Tmux config

---
+++ tmux/scripts/tmux-switch-session.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+current=$(tmux display-message -p '#S')
+sessions=$(tmux list-sessions -F '#S' | grep -v "^${current}$")
+
+if [ -z "$sessions" ]; then
+  echo "No other sessions available"
+  sleep 1
+  exit 0
+fi
+
+selected=$(echo "$sessions" | fzf --reverse)
+[ -n "$selected" ] && tmux switch-client -t "$selected"
